a7f6d725babd13a8b5b0bbc9123294c178d14e20
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-12 16:58:01 -0400

    Add github-run-script

---
+++ Formula/github-run-script.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class GithubRunScript < Formula
+  desc "Run a script on multiple repositories, cloning them if needed"
+  homepage "https://github.com/PythonCoderAS/github-run-script#readme"
+  url "https://registry.npmjs.org/github-run-script/-/github-run-script-1.0.0.tgz"
+  sha256 "d6641262d23d3f67c1f1c041e1476c74d441a5127834b097a2f766ce9e6acb6c"
+  license "MIT"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    raise "Test not implemented."
+  end
+end
