0dd0655ce74be694ebc1950777ef44b4ee0df54e
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-13 20:23:37 -0400

    github-run-script 1.0.3

--- Formula/github-run-script.rb
+++ Formula/github-run-script.rb
@@ -3,8 +3,8 @@ require "language/node"
 class GithubRunScript < Formula
   desc "Run a script on multiple repositories, cloning them if needed"
   homepage "https://github.com/PythonCoderAS/github-run-script#readme"
-  url "https://registry.npmjs.org/github-run-script/-/github-run-script-1.0.2.tgz"
-  sha256 "168f4ff097567ac275ceb14df73767d08b36fc18631bed1ff046670b5c765f72"
+  url "https://registry.npmjs.org/github-run-script/-/github-run-script-1.0.3.tgz"
+  sha256 "7bcb70ca5c79c40c5eba795e07749e2d2c01caf80f8b6b403009fc3531ea3501"
   license "MIT"
 
   livecheck do
